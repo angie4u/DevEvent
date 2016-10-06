c58d2c3c08df95d399853a793f0d795180fcc6f5
angie4u <eunk.angie@outlook.com>
2016-10-06 12:32:56 +0900

    first Commit

---
+++ Backend/DevEvent.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevEvent.Web.Startup))]
+namespace DevEvent.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
