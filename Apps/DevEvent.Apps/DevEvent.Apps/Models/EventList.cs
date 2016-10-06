513cc548e1ceef245326bf04599df0871ecd7002
angie4u <eunk.angie@outlook.com>
2016-10-06 17:41:23 +0900

    Event List Page Update

---
+++ Apps/DevEvent.Apps/DevEvent.Apps/Models/EventList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevEvent.Apps.Models
+{
+    public class EventList
+    {        
+        public int ID { get; set; }
+        public string EventTitle { get; set; }
+        public DateTime EventStartDay { get; set; }
+        public DateTime EventEndDay { get; set; }
+        public string Venue { get; set; }
+        public double locationX { get; set; }
+        public double locationY { get; set; }
+        public string Audience { get; set; }
+        public string Description { get; set; }
+        public string Agenda { get; set; }
+        public string Speaker { get; set; }
+        public string RegistrationURL { get; set; }
+        public string ContentsURL { get; set; }
+        public int ImageNumber { get; set; }
+        
+
+
+    }
+}
--- Apps/DevEvent.Apps/DevEvent.Apps/ViewModels/MainViewModel.cs
+++ Apps/DevEvent.Apps/DevEvent.Apps/ViewModels/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using DevEvent.Apps.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,50 @@ namespace DevEvent.Apps.ViewModels
         NavigationService navigationService;
         ApiService apiService;
 
+
+
         public MainViewModel()
         {
             navigationService = new NavigationService();
             apiService = new ApiService();
+
+            LoadMenu();
+
+        }
+
+        public ObservableCollection<MenuItemViewModel> Menu { get; set; }
+
+        private void LoadMenu()
+        {
+            Menu = new ObservableCollection<MenuItemViewModel>();
+
+            Menu.Add(new MenuItemViewModel()
+            {
+                Icon = "ic_action_home",
+                Title = "Home",
+                PageName = "MainPage"
+            });
+
+            Menu.Add(new MenuItemViewModel()
+            {
+                Icon = "ic_action_home",
+                Title = "지난행사",
+                PageName = "MainPage"
+            });
+
+            Menu.Add(new MenuItemViewModel()
+            {
+                Icon = "ic_action_home",
+                Title = "관심행사",
+                PageName = "MainPage"
+            });
+
+            Menu.Add(new MenuItemViewModel()
+            {
+                Icon = "ic_action_facebook",
+                Title = "Microsoft Developer",
+                PageName = "Microsoft Developer"
+            });
 
 
         }
