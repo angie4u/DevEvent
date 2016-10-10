eb3d02ef589db3e0261c3414318f1c5a348f9437
ilseokoh <onestone@live.co.kr>
2016-10-11 05:45:24 +0900

    Event Creation UI

--- Backend/DevEvent.Web/App_Start/BundleConfig.cs
+++ Backend/DevEvent.Web/App_Start/BundleConfig.cs
@@ -8,7 +8,8 @@ namespace DevEvent.Web
         // 번들 작성에 대한 자세한 내용은 http://go.microsoft.com/fwlink/?LinkId=301862 링크를 참조하십시오.
         public static void RegisterBundles(BundleCollection bundles)
         {
-           
+            bundles.Add(new ScriptBundle("~/js/views/adminevent/create").Include(
+                         "~/js/views/adminevent-create.js"));
         }
     }
 }
---
+++ Backend/DevEvent.Web/js/views/adminevent-create.js
@@ -0,0 +1,35 @@
+﻿(function ($) {
+    'use strict'
+    $('#startdate').datetimepicker({ format: 'YYYY-MM-DD h:mm:ss a', sideBySide: true });
+    $('#enddate').datetimepicker({ format: 'YYYY-MM-DD h:mm:ss a', sideBySide: true });
+    // enddate > startdate
+
+    var geocoder;
+    var markers = [];
+    var map;
+    geocoder = new google.maps.Geocoder();
+    map = new google.maps.Map(document.getElementById("map"), { zoom: 16, center: { lat: 37.566535, lng: 126.97796919999996 }, scrollwheel: false });
+    function setMapOnAll(map) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+        }
+    }
+
+    $('#showonmap').click(function () {
+        setMapOnAll(null);
+        var address = $('#address').val();
+        geocoder.geocode({ 'address': address }, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                map.setCenter(results[0].geometry.location);
+                
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: results[0].geometry.location
+                });
+                markers.push(marker);
+            } else {
+                alert("Geocode was not successful for the following reason: " + status);
+            }
+        });
+    });
+})(this.jQuery);
